3f6a20475542bff7fd7b3ffa56c3600e449a7841
James Williams <amascuba@gmail.com>
2021-11-23 11:57:20 -0600

    fix tags page

---
+++ scripts/fix_date.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import re
+import os
+
+dirs = ["../_posts", "../_drafts"]
+
+for dir in dirs:
+    os.chdir(dir)
+
+    for item in os.listdir():
+        if os.path.isfile(item):
+            with open(item) as f:
+                data = re.sub("(\d{4}-\d{1,2}-\d{1,2})(\w\d+:\d+\d:\d+\.\d+-\d+:\d+)","\\1", f.read())
+
+            with open(item, "w") as f:
+                f.write(data)
